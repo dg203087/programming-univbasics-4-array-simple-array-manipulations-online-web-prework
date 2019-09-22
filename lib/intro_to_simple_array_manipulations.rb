f9de623ebcc4c2842ab9a17f9a12ebffdd440033
Deanna Kelley <deanna.georges@gmail.com>
2019-09-22 02:18:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,7 +11,6 @@ def using_unshift(array, string)
 end
 
 def using_pop(array)
-  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
   deleted_string = continents.pop
 end
 
